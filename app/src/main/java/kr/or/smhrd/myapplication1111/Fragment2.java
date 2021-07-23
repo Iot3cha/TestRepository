b371cadf72160127b2ca2189b589a2ac5581b213
Iot3cha <cjh19960923@nate.com>
2021-07-23 17:38:15 +0900

    안드로이드 앱 커밋

---
+++ app/src/main/java/kr/or/smhrd/myapplication1111/Fragment2.java
@@ -0,0 +1,30 @@
+package kr.or.smhrd.myapplication1111;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Fragment2 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+       
+        TextView fg1_tv_data =  view.findViewById(R.id.fg2_tv_data);
+
+        fg1_tv_data.setText("두 번째 프래그먼트 화면");
+
+
+        return view;
+    }
+}
